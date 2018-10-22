119be8729936f8168eb491ebc19077a6901e06cf
Donaldo Puentes <dony9324@hotmail.com>
2018-10-21 22:35:12 -0500

    segundo comint para migrar
    la base de datos esta actualisada pero faltan crear algunos campos en la tabla sell

--- core/app/action/presentaciones-action.php
+++ core/app/action/presentaciones-action.php
@@ -1,13 +1,63 @@
-<div class="box-body">
-              <div class="row">
-                <div class="col-xs-3"> = 
-                  <input type="text" class="form-control" placeholder=".col-xs-3">
-                </div>
-                <div class="col-xs-4"> =
-                  <input type="text" class="form-control" placeholder=".col-xs-4">
-                </div>
-                <div class="col-xs-5"> =
-                  <input type="text" class="form-control" placeholder=".col-xs-5">
-                </div>
-</div>
+<div class="nav-tabs-custom">
+  <ul class="nav nav-tabs pull-right">
+    <li class=""><a href="#tab_1-1" data-toggle="tab" aria-expanded="false">Grupos</a></li>
+    <li class=""><a href="#tab_2-2" data-toggle="tab" aria-expanded="false">Fracciones</a></li>
+    <li class="active"><a href="#tab_3-2" data-toggle="tab" aria-expanded="true">Instrucciones</a></li>
+    <li class="pull-left header"><i class="fa fa-th"></i>Presentaciones</li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane" id="tab_1-1">
+
+    </div>
+    <!-- /.tab-pane -->
+    <div class="tab-pane" id="tab_2-2">
+      and regular than that of the individual languages.
+    </div>
+    <!-- /.tab-pane -->
+    <div class="tab-pane active" id="tab_3-2">
+      <div class="panel box box-primary">
+        <div class="box-header with-border">
+          <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" class="collapsed">
+              Agrupar
+            </a>
+          </h4>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse">
+          <div class="box-body">
+            <p>Debes ingresar la cantidad a agrupar y nombrar una unidad de medida. Ejemplo: tenemos un producto que se vende por caja de 12 y por unidades. Entonces la cantidad a agrupar seria “12” y la unidad seria “docenas”. Otro ejemplo: si vendo galletas en paquetes de 10, pero al mismo tiempo vendo en unidades, entonces la cantidad a agrupar seria “10” y la unidad seria “paquetes”</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="panel box box-primary">
+        <div class="box-header with-border">
+          <h4 class="box-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false">
+              Fraccionar
+            </a>
+          </h4>
+        </div>
+        <div id="collapseTwo" class="panel-collapse collapse">
+          <div class="box-body">
+            <p>Debes ingresar las Fracciones que contendrá tu producto, no necesariamente con la base 10,
+              ya que hay fracciones de pares, docena, cientos, kilos y cualquier otra medida que pueda existir como 1/8 de galón
+              o 1/4 de galón y otras medidas. Luego nombrar una unidad de medida, generalmente seria la misma de la medida principal. Ejemplo:
+              tenemos un tubo de PVC que mide 6 metros entero, pero también se vende por metros; (un metro, dos metros etcétera). Entonces las fracciones seria 6
+              y la unidad seria “metros”.
+              Otro ejemplo
+              Bolsa de cemento de 40 kilogramos, si solo se vende en unidades no es necesario fraccionarla.
+              Si se vende media bolsa las fracciones serian 2 y la unidad seria bolsa.
+              Si se vende por kilogramos las fracciones serian 40 y la unidad seria “kilogramos”
+              Y si vende medio kilo hay dos posibilidades vender 0.5 de un kilo o fraccionar a 80. (Vender 0.5 solo se recomienda en medidas en base 10)
+              Otro ejemplo: si vendo galletas en paquetes de 10, pero al mismo tiempo vendo en unidades, entonces la cantidad de Fracciones seria “10” y la unidad seria “unidades” en este caso se toma como medida principal “paquete” y se fracciona lo cual no es recomendable es mejor utilizar como unidad principal “unidades” y luego agrupar en paquetes de 10, pero si el proveedor los entrega por paquetes y no se fraccionaran las unidades, también es válido.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- /.tab-pane -->
+  </div>
+  <!-- /.tab-content -->
 </div>
